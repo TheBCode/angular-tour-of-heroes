de7de56ccc7b494a83952f01e5d221db2c9af2c4
Byron Buyck <bbuyck1@gmail.com>
2021-10-07 18:50:47 -0400

    Trying to add all files

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Dr. Strange'},
+  {id: 12, name: 'Flame'},
+  {id: 13, name: 'Bomberman'},
+  {id: 14, name: 'Constantine'},
+  {id: 15, name: 'Magneto'},
+  {id: 16, name: 'Juggernaut'},
+  {id: 17, name: 'Dr. Doom'},
+  {id: 18, name: 'Franklin Richards'},
+  {id: 19, name: 'Dynamo'},
+  {id: 20, name: 'Storm'}
+];
+
